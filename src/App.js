c40e9d56652adf114a1e35a79f7ab970b742308e
shivanigoyal1799 <shivanigoyal.mca19.du@gmail.com>
2021-08-07 12:06:18 +1200

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>Welcome to React Parcel Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+    </React.Fragment>
+  );
+};
+export default App;
