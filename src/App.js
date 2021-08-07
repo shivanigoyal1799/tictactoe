6ec184b4f17c3260048a8d1f7309b9202b379427
shivanigoyal1799 <shivanigoyal.mca19.du@gmail.com>
2021-08-07 17:52:18 +1200

    created board

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-
+import React from 'react';
+import Board from './components/Board';
 const App = () => {
   return (
-    <React.Fragment>
-      <h1>Welcome to React Parcel Micro App!</h1>
-      <p>Hard to get more minimal than this React app.</p>
-    </React.Fragment>
+    <div>
+      <h1>TIC TAC TOE</h1>
+      <Board />
+    </div>
   );
 };
 export default App;
