35ed43a19075d45a17f957ad791679d26613c329
shivanigoyal1799 <shivanigoyal.mca19.du@gmail.com>
2021-08-14 18:16:03 +0530

    displayed draw message using conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
 import History from './components/History';
+import StatusMessage from './components/StatusMessage';
 import { calculateWinner } from './components/helper';
 import './styles/root.scss';
 
@@ -13,12 +14,9 @@ const App = () => {
   ]);
   const [currMove, setCurrMove] = useState(0);
   const current = history[currMove];
-  console.log('history', history);
 
   const winner = calculateWinner(current.board);
-  const message = winner
-    ? `Winner is ${winner}`
-    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
+
   const handleSquareClick = position => {
     if (current.board[position] || winner) return;
     setHistory(prev => {
@@ -40,7 +38,7 @@ const App = () => {
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <h2>{message}</h2>
+      <StatusMessage winner={winner} current={current} />
       <Board
         initialBoardState={current.board}
         handleSquareClick={handleSquareClick}
---
+++ src/components/StatusMessage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const StatusMessage = ({ winner, current }) => {
+  const noMoveLeft = current.board.every(elem => elem != null);
+  return (
+    <h2>
+      {/* conditional rendering */}
+      {winner && `Winner is ${winner}`}
+      {!winner &&
+        !noMoveLeft &&
+        `Next player is ${current.isXNext ? 'X' : 'O'}`}
+      {!winner && noMoveLeft && `Draw !!`}
+    </h2>
+  );
+};
+
+export default StatusMessage;
